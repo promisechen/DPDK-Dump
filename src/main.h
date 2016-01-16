4a34b81fd1e528a108cc75a58d520cd6816ed606
promisechen <chenlx10@163.com>
2016-01-16 21:30:00 +0800

    Update main.h

--- src/main.h
+++ src/main.h
@@ -108,7 +108,7 @@ static const struct rte_eth_conf port_conf = {
 		.rss_conf = {
 			.rss_key = rss_seed,				/* Set the seed,					*/
 			.rss_key_len = 40,				/* and the seed length.					*/
-			.rss_hf = (ETH_RSS_IPV4_TCP | ETH_RSS_UDP) ,	/* Set the mask of protocols RSS will be applied to 	*/
+			.rss_hf = (ETH_RSS_NONFRAG_IPV4_TCP | ETH_RSS_UDP) ,	/* Set the mask of protocols RSS will be applied to 	*/
 		}	
 	}
 };
